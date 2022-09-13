641dd38bb2b7f0b15d1d03820c69c0abb37b0fd0
Chris West (Faux) <git@goeswhere.com>
2022-09-13 20:50:49 +0100

    feat: persistent connection

--- examples/neohub.rs
+++ examples/neohub.rs
@@ -1,13 +1,32 @@
+use std::fs;
+use std::io::Write;
+use std::time::{Duration, SystemTime};
+
 use anyhow::Result;
+
+fn now() -> u128 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("it's not the past")
+        .as_millis()
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
-    let client = neohub::Client::new_host("192.168.178.37")?;
-    // let bytes = client.raw_message(b"{\"INFO\":0}")?;
-    // let bytes = String::from_utf8_lossy(&bytes);
-    // let value: Value = serde_json::from_str(bytes.as_ref())?;
-    // println!("{:#?}", value);
-    println!("{:?}", client.get_profiles().await?);
-    Ok(())
+    let unix_millis = now();
+    let writer = fs::File::create(format!("logs.{unix_millis}.jsonl.zstd"))?;
+    let mut writer = zstd::Encoder::new(writer, 9)?;
+    let mut client = neohub::Client::new(
+        "wss://192.168.178.37:4243",
+        "7764994a-e10f-4f5e-bdb0-14b8861dcdc3",
+    )?;
+    loop {
+        let live_data = client.get_live_data().await?;
+        writer.write_all(format!("{} ", now()).as_bytes())?;
+        serde_json::to_writer(&mut writer, &live_data)?;
+        writer.write_all(b"\n")?;
+        writer.flush()?;
+        std::thread::sleep(Duration::from_secs(5));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,45 @@
 use std::collections::HashMap;
-use std::net::{IpAddr, SocketAddr};
-use std::str::FromStr;
 
-use anyhow::Result;
+use anyhow::{anyhow, ensure, Context, Result};
 use futures_util::{SinkExt, StreamExt};
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_json::{json, Value};
+use tokio::net::TcpStream;
 use tokio_tungstenite::tungstenite::Message;
-use tokio_tungstenite::{connect_async_tls_with_config, Connector};
+use tokio_tungstenite::{
+    connect_async_tls_with_config, Connector, MaybeTlsStream, WebSocketStream,
+};
+
+type WsStream = WebSocketStream<MaybeTlsStream<TcpStream>>;
 
 pub struct Client {
-    host: SocketAddr,
+    url: String,
+    token: String,
+    conn: Option<WsStream>,
 }
 
 impl Client {
-    pub fn new_host(host: &str) -> Result<Self> {
-        Ok(Self::new_addr((IpAddr::from_str(host)?, 4243)))
+    pub fn new(url: impl ToString, token: impl ToString) -> Result<Self> {
+        Ok(Client {
+            url: url.to_string(),
+            token: token.to_string(),
+            conn: None,
+        })
     }
 
-    pub fn new_addr(host: impl Into<SocketAddr>) -> Self {
-        Client { host: host.into() }
+    #[inline]
+    async fn ensure_connected(&mut self) -> Result<()> {
+        if self.conn.is_none() {
+            self.conn = Some(connect(&self.url).await?);
+        }
+        Ok(())
     }
 
-    pub async fn raw_message(&self, msg: &str) -> Result<String> {
-        let connector = Connector::NativeTls(
-            native_tls::TlsConnector::builder()
-                .danger_accept_invalid_certs(true)
-                .build()?,
-        );
-        let (conn, _) =
-            connect_async_tls_with_config("wss://192.168.178.37:4243", None, Some(connector))
-                .await?;
-        let (mut write, mut read) = conn.split();
-        // write.feed(Message::Close(None)).await?;
-        // write.flush().await?;
-        // 7764994a-e10f-4f5e-bdb0-14b8861dcdc3
+    pub async fn raw_message(&mut self, msg: &str) -> Result<String> {
         let middle = serde_json::to_string(&json!({
-            "token": "7764994a-e10f-4f5e-bdb0-14b8861dcdc3",
+            "token": self.token,
             "COMMANDS": [
                 { "COMMAND": msg, "COMMANDID": 1, }
             ]
@@ -47,46 +48,57 @@ impl Client {
             "message_type": "hm_get_command_queue",
             "message": middle,
         });
-        let line = serde_json::to_string(&outer)?;
-        println!("{}", line);
-        write.feed(Message::Text(line)).await?;
-        write.flush().await?;
-        // write.write_all(msg)?;
-        // write.write_all(b"\0\r")?;
-        // write.flush()?;
-        // write.shutdown(Shutdown::Write)?;
-        // let mut conn = io::BufReader::new(read);
-        // let mut buf = Vec::with_capacity(4096);
-        // conn.read_until(0, &mut buf)?;
-        // while !buf.is_empty() && [b'\r', b'\n', b'\0'].contains(&buf[buf.len() - 1]) {
-        //     buf.pop();
-        // }
-        let buf = read
+        let to_send = serde_json::to_string(&outer)?;
+
+        self.ensure_connected().await?;
+        let conn = self
+            .conn
+            .as_mut()
+            .expect("ensure_connected contract violated");
+
+        conn.feed(Message::Text(to_send)).await?;
+        conn.flush().await?;
+
+        let buf = conn
             .next()
             .await
-            .expect("some")
-            .expect("success")
+            .ok_or_else(|| anyhow!("no response received to command"))?
+            .with_context(|| "unpacking websocket message")?
             .into_data();
-        let resp: Value = serde_json::from_slice(&buf)?;
-        let buf = resp
-            .as_object()
-            .expect("response object")
-            .get("response")
-            .expect("response present")
-            .as_str()
-            .expect("response string")
-            .to_string();
-        Ok(buf)
+        let resp: CommandResponse =
+            serde_json::from_slice(&buf).with_context(|| "JSON-deserializing response")?;
+        ensure!(
+            resp.message_type == "hm_set_command_response",
+            "unexpected response type: {:?}",
+            resp
+        );
+        Ok(resp.response)
     }
 
-    pub async fn command_void<T: DeserializeOwned>(&self, command: &str) -> Result<T> {
+    pub async fn command_void<T: DeserializeOwned>(&mut self, command: &str) -> Result<T> {
         let resp = self.raw_message(&format!("{{'{}':0}}", command)).await?;
         Ok(serde_json::from_str(&resp)?)
     }
 
-    pub async fn get_profiles(&self) -> Result<HashMap<String, Profile>> {
+    pub async fn get_profiles(&mut self) -> Result<HashMap<String, Profile>> {
         Ok(self.command_void("GET_PROFILES").await?)
     }
+
+    pub async fn get_live_data(&mut self) -> Result<Value> {
+        Ok(self.command_void("GET_LIVE_DATA").await?)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct CommandResponse {
+    // command_id: i64,
+    // device_id: "mac address",
+
+    // hm_set_command_response
+    message_type: String,
+
+    // json, in a string
+    response: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -121,10 +133,12 @@ pub struct ProfileInfoDay {
     sleep: TempSpec,
 }
 
-// #[derive(Deserialize, Serialize, Debug)]
-// pub struct ProfileInfoDay {
-//     pub time: String,
-//     pub set_temp: f32,
-//     five: u16,
-//     troo: bool,
-// }
+async fn connect(url: &str) -> Result<WsStream> {
+    let connector = Connector::NativeTls(
+        native_tls::TlsConnector::builder()
+            .danger_accept_invalid_certs(true)
+            .build()?,
+    );
+    let (mut conn, _) = connect_async_tls_with_config(url, None, Some(connector)).await?;
+    Ok(conn)
+}
